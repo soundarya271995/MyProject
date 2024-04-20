d1366eabe4379d61223fc9f1e320594d80044221
soundarya271995 <ssoundu95@gmail.com>
2024-04-20 15:32:55 +0530

    triple

--- src/com/git/Sample.java
+++ src/com/git/Sample.java
@@ -4,15 +4,19 @@ public class Sample {
 private void method1() {
 	System.out.println("completed");
 }private void method2() {
-	System.out.println("completed3");
+	System.out.println("completed2");
 }
 private void method3() {
-	System.out.println("completed2");
+	System.out.println("completed3");
+}
+private void method4() {
+	System.out.println("completed1");
 }
 public static void main(String[] args) {
 	Sample s = new Sample();
 	s.method1();
 	s.method2();
 	s.method3();
+	s.method4();
 }
 }
