d5616e65f125d521d703e559f6e9441274b430bc
soundarya271995 <ssoundu95@gmail.com>
2024-04-20 14:25:56 +0530

    updated

--- src/com/git/Sample.java
+++ src/com/git/Sample.java
@@ -7,9 +7,13 @@ private void method1() {
 private void method2() {
 	System.out.println("completed1");
 }
+private void method3() {
+	System.out.println("completed2");
+}
 public static void main(String[] args) {
 	Sample s = new Sample();
 	s.method1();
 	s.method2();
+	s.method3();
 }
 }
