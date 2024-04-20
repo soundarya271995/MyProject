c0341e0dc3a3c8927eb3c34ca094ed0cefd6baa6
soundarya271995 <ssoundu95@gmail.com>
2024-04-20 12:36:20 +0530

    new code

---
+++ src/com/git/Sample.java
@@ -0,0 +1,11 @@
+package com.git;
+
+public class Sample {
+private void method1() {
+	System.out.println("completed");
+}
+public static void main(String[] args) {
+	Sample s = new Sample();
+	s.method1();
+}
+}
