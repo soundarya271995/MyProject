b1fcf4886fc7520ba1e22734bf4a6e7ccce0420a
soundarya271995 <ssoundu95@gmail.com>
2024-04-20 14:21:41 +0530

    code

--- src/com/git/Sample.java
+++ src/com/git/Sample.java
@@ -4,8 +4,12 @@ public class Sample {
 private void method1() {
 	System.out.println("completed");
 }
+private void method2() {
+	System.out.println("completed1");
+}
 public static void main(String[] args) {
 	Sample s = new Sample();
 	s.method1();
+	s.method2();
 }
 }
